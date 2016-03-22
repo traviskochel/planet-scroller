0777873d3242d47fcf0d140e49b42190c0c18aed
Travis Kochel <travis@scribbletone.com>
2016-03-22 13:09:50 -0700

    works

--- src/Components/PlanetWrapper.js
+++ src/Components/PlanetWrapper.js
@@ -1,11 +1,19 @@
 var React = require('react');
+var classnames = require('classnames');
 
 var PlanetWrapper = React.createClass({
 
   render: function() {
     return (
       <div className='planet-wrapper' >
-        <img src='/dist/images/Jupiter2.svg' className='planet-img' />
+        <img 
+          key='img-1'
+          className='planet-img planet-img--1'
+          src='/dist/images/Jupiter2.svg'/>
+        <img 
+          key='img-2'
+          className='planet-img planet-img--2'
+          src='/dist/images/Jupiter2.svg'/>
       </div>
     );
   }
