a9883da48d81b065b40663509fdd92ed661e10f5
Travis Kochel <travis@scribbletone.com>
2016-03-22 13:22:22 -0700

    backdrop

--- src/Components/PlanetWrapper.js
+++ src/Components/PlanetWrapper.js
@@ -6,14 +6,17 @@ var PlanetWrapper = React.createClass({
   render: function() {
     return (
       <div className='planet-wrapper' >
-        <img 
-          key='img-1'
-          className='planet-img planet-img--1'
-          src='/dist/images/Jupiter2.svg'/>
-        <img 
-          key='img-2'
-          className='planet-img planet-img--2'
-          src='/dist/images/Jupiter2.svg'/>
+        <div className='planet-backdrop' />
+        <div className='planet-img-container'>
+          <img 
+            key='img-1'
+            className='planet-img planet-img--1'
+            src='/dist/images/Jupiter2.svg'/>
+          <img 
+            key='img-2'
+            className='planet-img planet-img--2'
+            src='/dist/images/Jupiter2.svg'/>
+        </div>
       </div>
     );
   }
